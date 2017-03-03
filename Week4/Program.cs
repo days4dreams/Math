7d33aa6b817c9064aae3fa42e53bd4b39e9bfd7e
lauren.wilson <lauren.wilson@SAGE016682>
2017-03-03 14:20:55 +0000

    Add project files.

---
+++ Week4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius = 10;
+            double radius2 = 5;
+            double calculation = 0;
+            double calculation2 = 0;
+            calculation = Area(radius);
+            calculation2 = Circum(radius2);
+
+            Console.WriteLine(calculation);
+            Console.WriteLine(calculation2);
+            Console.ReadKey();
+        }
+        static double Area(double r)
+        {
+            double area = 0;
+            area = Math.PI * Math.Pow(r, 2);
+            return area;
+        }
+
+        static double Circum(double r)
+        {
+            double c = 0;
+            c = 2 * Math.PI * r;
+            return c;
+        }
+
+    }
+}
